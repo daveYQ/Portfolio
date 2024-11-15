64f85b6922ba6bfad4d9f927327c070afdbf5c23
daveq <110788152+daveYQ@users.noreply.github.com>
2024-11-15 10:53:40 +0100

    added base to link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daveYQ.github.io',
-  base: '',
+  base: 'portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 })
